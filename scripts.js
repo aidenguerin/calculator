432a45bc4a99500381596c134c54a968264a7ab6
Aiden Guerin <90363998+aidenguerin@users.noreply.github.com>
2023-12-29 22:30:27 +1100

    added calculator logic

--- scripts.js
+++ scripts.js
@@ -1,30 +1,100 @@
-let num1 = 0;
-let num2 = 0;
+let storedValue = "";
+let currentValue = "";
 let operator = "";
 
-// BASIC FUNCTIONS
-function add(a,b) {
-    return a + b;
+function deleteFromDisplay() {
+    currentValue = currentValue.toString().slice(0,-1);
+
+    updateDisplay();
 }
 
-function subtract(a,b) {
-    return a - b;
+function clearDisplay() {
+    storedValue = "";
+    currentValue = "";
+    operator = "";
+
+    updateDisplay();
 }
 
-function divide(a,b) {
-    return a / b;
+function updateDisplay() {
+    displayCurrentValue.innerText = currentValue;
+    displayStoredValue.innerText = `${storedValue} ${operator}`;
 }
 
-function multiply(a,b) {
-    return a * b;
+function setOperator(inputValue) {
+    operator = inputValue.toString();
 }
 
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case "+" : return add(num1, num2);
-        case "-" : return subtract(num1, num2);
-        case "*" : return multiply(num1, num2);
-        case "/" : return divide(num1, num2);
-        default : return "ERROR";
+function storeCurrentValue() {
+    storedValue = currentValue.toString();
+    currentValue = "";
+}
+
+function appendToCurrentValue(inputValue) {
+    if (currentValue.includes(".") && inputValue == ".") {
+        return
     }
+    currentValue = currentValue.toString() + inputValue.toString();
 }
+
+function calculate() {
+    let a = parseFloat(storedValue);
+    let b = parseFloat(currentValue);
+
+    switch (operator) {
+        case "+": 
+            currentValue = a + b;
+            break;
+
+        case "-":
+            currentValue = a - b;
+            break;
+
+        case "×":
+            currentValue = a * b;
+            break;
+        
+        case "÷":
+            currentValue = a / b;
+            break;
+    
+        default:
+            break;
+    }
+
+    storedValue = "";
+    operator = "";
+}
+
+const displayCurrentValue = document.querySelector("#current-value");
+const displayStoredValue = document.querySelector("#stored-value");
+
+
+const clearButton = document.querySelector("#clear-btn");
+clearButton.addEventListener('click', () => clearDisplay());
+
+const deleteButton = document.querySelector("#delete-btn");
+deleteButton.addEventListener('click', ()=> deleteFromDisplay());
+
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach(element => {
+    element.addEventListener('click', () => {
+        appendToCurrentValue(element.innerText);
+        updateDisplay();
+    });
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(element => {
+    element.addEventListener('click', () => {
+        setOperator(element.innerHTML);
+        storeCurrentValue();
+        updateDisplay();
+    });
+})
+
+const calculateButton = document.querySelector("#calculate");
+calculateButton.addEventListener('click', () => {
+    calculate();
+    updateDisplay();
+})
